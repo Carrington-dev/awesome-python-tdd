de7b75c938b7aebebaad3a893321ee73929f399f
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-12-02 12:38:37 +0200

    Update main.py

--- examples/with-unittest/calculator_project/main.py
+++ examples/with-unittest/calculator_project/main.py
@@ -14,3 +14,30 @@ logger = logging.getLogger(__name__)
 
 class Calculator:
     logger.info("Calculator initialized")
+
+    def add(self, a, b):
+        if not (isinstance(a, (int, float,)) and isinstance(b, (int, float))):
+            logger.error("Addition error: parameters must be numbers")
+            raise TypeError("Parameters must be numbers")
+        return a + b
+
+    def subtract(self, a, b):
+        if not (isinstance(a, (int, float,)) and isinstance(b, (int, float))):
+            logger.error("Subtraction error: parameters must be numbers")
+            raise TypeError("Parameters must be numbers")
+        return a - b
+
+    def multiply(self, a, b):
+        if not (isinstance(a, (int, float,)) and isinstance(b, (int, float))):
+            logger.error("Multiplication error: parameters must be numbers")
+            raise TypeError("Parameters must be numbers")
+        return a * b
+    
+    def divide(self, a, b):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            logger.error("Division error: parameters must be numbers")
+            raise TypeError("Parameters must be numbers")
+        if b == 0:
+            logger.error("Division error: division by zero")
+            raise ZeroDivisionError("Cannot divide by zero")
+        return a / b
