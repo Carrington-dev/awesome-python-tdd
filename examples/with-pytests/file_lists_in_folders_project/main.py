c8de52f3bed62de00b1f86e7fe24648abd78001f
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-12-02 13:51:28 +0200

    Update main.py

--- examples/with-pytests/file_lists_in_folders_project/main.py
+++ examples/with-pytests/file_lists_in_folders_project/main.py
@@ -1,5 +1,16 @@
 class FolderLists:
-    pass
+    def __init__(self):
+        self.folders: list[Folder] = []
+        self.files: list[str] = []
+
+    def add_folder(self, folder: Folder):
+        self.folders.append(folder)
 
 class Folder:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.files = []
+        self.subfolders = []
+    
+    
+    
